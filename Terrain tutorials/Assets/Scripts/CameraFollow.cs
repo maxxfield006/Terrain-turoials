e03f59ee64528bf986b73f52c00c71e3ec62cedf
Max Field <maxfield7766@gmail.com>
2023-02-11 23:37:41 +1300

    Saturday Work

---
+++ Terrain tutorials/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Net.Http.Headers;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject car;
+    public Vector3 offset = new Vector3(0, 3, -5);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = car.transform.position + offset;
+    }
+}
